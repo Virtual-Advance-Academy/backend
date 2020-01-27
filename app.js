feec5ab82790147614fcd5fdc149d4b0d61993bb
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-01-27 01:14:56 -0500

    Cleaned up some code and packages --------- - Removed Jade as our backend API server will not be rendering pages - Removed views and assets due to previously mentioned reason - Removed index route as it was unnecessary

--- app.js
+++ app.js
@@ -4,22 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Base Routes
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -35,7 +29,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send("error");
 });
 
 module.exports = app;
