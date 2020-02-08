9b2fdfd07aa1eb94dd6cb7d0e71103cd36818486
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-02-08 04:35:38 -0500

    Added utility methods to user schema, lowercased emails

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
-let mongoose = require('mongoose')
-let Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     fullName: {
@@ -17,9 +19,10 @@ const userSchema = new Schema({
         unique: true,
         lowercase: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
+        lowercase: true,
         unique: true,
         trim: true
     },
@@ -28,5 +31,28 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    // Generate an auth token for the user
+    const user = this
+    const token = jwt.sign({ _id: user._id, name: user.fullName }, process.env.JWT_TOKEN)
+    return token
+}
+
+userSchema.statics.verifyUser = async (emailOrUsername, password) => {
+    emailOrUsername = emailOrUsername.toLowerCase()
+    // Search for a user by email and password.
+    let user = await User.findOne({ 
+        $or: [{email: emailOrUsername}, { username_lower: emailOrUsername }] 
+    })
+    if (!user) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password)
+    if (!isPasswordMatch) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
