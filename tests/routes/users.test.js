b7e7a445067ec0f97d326fd8407b011e7a590f66
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-04-25 04:00:53 -0400

    Fixed test leakage

--- tests/routes/users.test.js
+++ tests/routes/users.test.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
-const goodUser = require('./goodUser')
+const goodUser = require("./goodUser");
 const app = require("../../src/app");
-const api = request(app);
+let api = request(app);
+const mongoose = require("mongoose");
 
 describe("User endpoints", () => {
     it("should create a new User", async () => {
@@ -21,12 +22,11 @@ describe("User endpoints", () => {
         dupeUser = {
             ...goodUser,
             username: "immaDupe", // Both username and email must be unique
-            email: "different@email.com"
+            email: "different@email.com",
         };
 
         const res2 = await api.post("/users").send(dupeUser);
         expect(res2.status).toBe(400);
-
 
         dupeUser = {
             ...goodUser,
@@ -35,7 +35,6 @@ describe("User endpoints", () => {
         };
         const res3 = await api.post("/users").send(dupeUser);
         expect(res3.status).toBe(400);
-
     });
 
     it("should NOT create users that do not pass validation", async () => {
@@ -78,80 +77,80 @@ describe("User endpoints", () => {
         };
         const res5 = await api.post("/users").send(badUser);
         expect(res5.status).toBe(400);
-
     });
 
-    it('should NOT authenticate non-existant users', async () => {
-        const {password} = goodUser
+    it("should NOT authenticate non-existant users", async () => {
+        const { password } = goodUser;
         const authUser = {
             username: "DoIEvenExist",
-            password
-        }
-        
+            password,
+        };
+
         const res1 = await api.post("/users").send(authUser);
         expect(res1.status).toBe(400);
-    })
-    
-    it('should NOT authenticate users with invalid credentials', async () => {
+    });
 
+    it("should NOT authenticate users with invalid credentials", async () => {
         let authUser = {
             ...goodUser,
             username: "iForgotMypass",
-            email: 'iforgotmy@email.com'
-        }
+            email: "iforgotmy@email.com",
+        };
 
         const res1 = await api.post("/users").send(authUser);
         expect(res1.status).toBe(201);
 
-        const {username} = authUser;
+        const { username } = authUser;
         let authLogin = {
             username,
-            password: "iForgotIt"
-        }
+            password: "iForgotIt",
+        };
 
         const res2 = await api.post("/users").send(authLogin);
         expect(res2.status).toBe(400);
-    })
+    });
 
-    it('should authenticate users with valid credentials', async () => {
-
+    it("should authenticate users with valid credentials", async () => {
         let authUser = {
             ...goodUser,
             email: "otherAuth1@email.com",
-            username: "authMan1"
-        }
+            username: "authMan1",
+        };
 
         const res1 = await api.post("/users").send(authUser);
         expect(res1.status).toBe(201);
 
-        const {username, password} = authUser;
+        const { username, password } = authUser;
         let authLogin = {
             username,
-            password
-        }
+            password,
+        };
 
         const res2 = await api.post("/users").send(authLogin);
         expect(res2.status).toBe(400);
-    })
+    });
 
-    it('should authenticate users with either email OR username', async () => {
-
+    it("should authenticate users with either email OR username", async () => {
         let authUser = {
             ...goodUser,
             email: "otherAuth2@email.com",
-            username: "authMan2"
-        }
+            username: "authMan2",
+        };
 
         const res1 = await api.post("/users").send(authUser);
         expect(res1.status).toBe(201);
 
         authUser = {
             ...authUser,
-            password: "iForgotIt"
-        }
+            password: "iForgotIt",
+        };
 
         const res2 = await api.post("/users").send(authUser);
         expect(res2.status).toBe(400);
-    })
+    });
 
+    afterAll(async (done) => {
+        await mongoose.disconnect();
+        done()
+    });
 });
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,17 +1,20 @@
-const request = require('supertest')
-const app = require('../src/app')
-const api = request(app)
+const request = require("supertest");
+const app = require("../src/app");
+const api = request(app);
+const mongoose = require("mongoose");
 
-describe('Server Litmus test', () => {
+describe("Server Litmus test", () => {
+    it("should test that the environment is in test mode", () => {
+        expect(process.env.NODE_ENV).toBe("test");
+    });
 
+    it("should return 404 for the index address", async () => {
+        const res = await api.get("/");
+        expect(res.status).toBe(404);
+    });
 
-    it('should test that the environment is in test mode', () => {
-        expect(process.env.NODE_ENV).toBe('test')
-    })
-
-    it('should return 404 for the index address', async () => {
-        const res = await api.get('/');
-        expect(res.status).toBe(404)
-    })
-
-})
+    afterAll(async (done) => {
+        await mongoose.disconnect();
+        done()
+    });
+});
