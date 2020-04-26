259025a8b402539c05f89f7432ef12b037e7658d
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-04-26 16:36:29 -0400

    Changed env loader settings

--- envConfig.js
+++ envConfig.js
@@ -1,4 +1,7 @@
 const dotenv = require("dotenv");
-const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
+const envFile =
+    process.env.NODE_ENV && process.env.NODE_ENV !== "production"
+        ? `.env.${process.env.NODE_ENV}`
+        : ".env";
 console.log("env file: ", envFile);
 dotenv.config({ path: envFile });
